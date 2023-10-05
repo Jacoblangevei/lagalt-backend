3f20083cf591cb0f2294493556d5a2e0ede9deb5
Jacob Langevei <jacob-langevei@hotmail.com>
2023-10-05 13:06:10 +0200

    Test build

--- Lagalt-Backend/Program.cs
+++ Lagalt-Backend/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //app.Environment.IsDevelopment()
 
-// TESTEST23456
+// TESTEST234567
 
 app.UseSwagger();
 app.UseSwaggerUI();
