977f6e33a7b23a8cf85a6b01e8573a61ce0d9b3b
Jacob Langevei <jacob-langevei@hotmail.com>
2023-10-06 12:56:40 +0200

    Added keycloak service
    Added service builder for keycloak in Program.cs

--- Lagalt-Backend/Program.cs
+++ Lagalt-Backend/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+            {
+                using var client = new HttpClient();
+                var keyuri = builder.Configuration["TokenSecrets:KeyURI"];
+                var response = client.GetAsync(keyuri).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(responseString);
+                return keys.Keys;
+            },
+            ValidIssuers = new List<string>
+            {
+                builder.Configuration["TokenSecrets:IssuerURI"]
+            },
+            ValidAudience = "account",
+        };
+    });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
