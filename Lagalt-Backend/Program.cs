2396413447656621b09383eb1394c60f9580620d
Ida Christensen <chr.ida@hotmail.com>
2023-10-19 11:04:02 +0200

    changed program cs

--- Lagalt-Backend/Program.cs
+++ Lagalt-Backend/Program.cs
@@ -84,16 +84,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//app.Environment.IsDevelopment()
+app.Environment.IsDevelopment();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-//app.UseSwagger();
-//app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
