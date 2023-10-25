b040d71d186326b45deb370c1c665148c2302f8f
Ida Christensen <chr.ida@hotmail.com>
2023-10-25 14:20:04 +0200

    cors fix

--- Lagalt-Backend/Program.cs
+++ Lagalt-Backend/Program.cs
@@ -118,8 +118,7 @@ app.UseHttpsRedirection();
 app.UseCors(MyAllowSpesificOrigins);
 app.Use(async (context, next) =>
 {
-    context.Response.Headers.Add("Content-Security-Policy", "frame-ancestors 'self' https://best-lagalt-project-git-main-ken-pixel-source.vercel.app;");
-
+    context.Response.Headers.Add("Content-Security-Policy", "frame-ancestors 'self' https://best-lagalt-project-git-main-ken-pixel-source.vercel.app https://lemur-10.cloud-iam.com;");
     await next();
 });
 
